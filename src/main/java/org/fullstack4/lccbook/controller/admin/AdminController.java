2f71eb614116b2369ef22a17a38bc4b03baf63fc
JS <wow220713@gmail.com>
2024-04-29 11:14:11 +0900

    Admin 로그인 체크 유틸 추가

--- src/main/java/org/fullstack4/lccbook/controller/admin/AdminController.java
+++ src/main/java/org/fullstack4/lccbook/controller/admin/AdminController.java
@@ -3,17 +3,13 @@ package org.fullstack4.lccbook.controller.admin;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.fullstack4.lccbook.dto.AdminDTO;
-import org.fullstack4.lccbook.dto.LoginDTO;
-import org.fullstack4.lccbook.dto.MemberDTO;
 import org.fullstack4.lccbook.service.AdminServiceIf;
+import org.fullstack4.lccbook.util.CommonLoginCheck;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -24,16 +20,10 @@ import javax.servlet.http.HttpSession;
 @RequiredArgsConstructor
 public class AdminController {
     private final AdminServiceIf adminServiceIf;
+    private final CommonLoginCheck commonLoginCheck;
     @RequestMapping(value = "/main",method = RequestMethod.GET)
     public String adminMainGET(HttpServletRequest request, RedirectAttributes redirectAttributes){
-        HttpSession session = request.getSession();
-        if(session.getAttribute("adminDTO")==null){
-            redirectAttributes.addFlashAttribute("errorAlert","<script> alert('해당페이지는 관리자 로그인이 필요합니다.') </script>");
-            return "redirect:/admin/login";
-        }
-        else{
-            return "/admin/main";
-        }
+        return commonLoginCheck.adminCheck(request,redirectAttributes);
     }
     @RequestMapping(value = "/login",method = RequestMethod.GET)
     public void adminLoginGET(){
