18a421cf5f725234b1d7976f34ede1f91a79b8d6
jongyun <cjy205205@naver.com>
2024-05-02 21:06:46 +0900

    fetch : instock 재고 등록 , 리스트 출력, 페이징네이션

--- src/main/java/org/fullstack4/lccbook/controller/admin/AinstockController.java
+++ src/main/java/org/fullstack4/lccbook/controller/admin/AinstockController.java
@@ -2,10 +2,8 @@ package org.fullstack4.lccbook.controller.admin;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.fullstack4.lccbook.dto.DeliveryDTO;
-import org.fullstack4.lccbook.dto.PageRequestDTO;
-import org.fullstack4.lccbook.dto.PageResponseDTO;
-import org.fullstack4.lccbook.service.DeliveryServiceIf;
+import org.fullstack4.lccbook.dto.*;
+import org.fullstack4.lccbook.service.InstockServiceIf;
 import org.fullstack4.lccbook.util.CommonLoginCheck;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,7 +11,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,11 +23,11 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class AinstockController {
 
-    private final DeliveryServiceIf deliveryService;
+    private final InstockServiceIf instockServiceIf;
     private final CommonLoginCheck commonLoginCheck;
 
     @GetMapping(value ="/list")
-    public String adeliveryList(@Valid PageRequestDTO pageRequestDTO
+    public String ainstockList(@Valid PageRequestDTO pageRequestDTO
             , BindingResult bindingResult
             , RedirectAttributes redirectAttributes
             , Model model
@@ -40,17 +37,18 @@ public class AinstockController {
         if(session.getAttribute("adminDTO")==null) {
             return commonLoginCheck.adminCheck(request, redirectAttributes);
         }
-
         if (bindingResult.hasErrors()) {
             log.info("BookController >> list errors");
             redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
         }
-        PageResponseDTO<DeliveryDTO> responseDTO = deliveryService.bbsListByPage(pageRequestDTO);
+        System.out.println("pageRequestDTO  controller: " + pageRequestDTO);
+        PageResponseDTO<InstockDTO> responseDTO = instockServiceIf.bbsListByPage(pageRequestDTO);
         model.addAttribute("responseDTO", responseDTO);
-        return "/admin/adelivery/list";
+        System.out.println("responseDTO ainstockList controller: " + responseDTO.toString());
+        return "/admin/ainstock/list";
 
     }
-
+/*
     @GetMapping(value = "/view")
     public void deliveryView(Model model,
                              @RequestParam(name="delivery_idx", defaultValue = "0") int delivery_idx){
@@ -74,7 +72,25 @@ public class AinstockController {
             return "/admin/adelivery/modify?delivery_idx=" + deliveryDTO.getDelivery_idx();
         }
 
+    }*/
+
+
+    @PostMapping("/regist")
+    public String registPOST(@Valid InstockDTO instockDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes, HttpServletRequest request){
+        HttpSession session = request.getSession();
+        if(session.getAttribute("adminDTO")==null) {
+            return commonLoginCheck.adminCheck(request, redirectAttributes);
+        }
+        System.out.println("instock controller : " + instockDTO.toString());
+     /*   if(bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("errors",bindingResult.getAllErrors());
+            return "redirect:/admin/adminmember/regist";
+        }*/
+        int result = instockServiceIf.regist(instockDTO);
+        return "redirect:/admin/ainstock/list";
+
     }
+
 
 }
 
